46065d12108b3e7ab6f1eadc3b17d2a309acd70a
Abdullah Shuja <shujaa@aston.ac.uk>
2018-02-19 20:08:01 +0000

    lab 2

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("index.js executing")
+var express = require('express');
+var app = express();
+var router = require('./routes/hello.js')
+
+var port = 3000;
+app.listen(port, function() {
+  console.log("Listening on port: " + port);
+});
+
+app.use('/', router);
